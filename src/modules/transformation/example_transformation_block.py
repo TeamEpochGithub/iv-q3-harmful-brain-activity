84df763d5dedb72d1da9b96ffb4347b10e584d2a
schobbejak <jmvanselm@gmail.com>
2024-03-04 16:02:22 +0100

    Add example training and transformation blocks

---
+++ src/modules/transformation/example_transformation_block.py
@@ -0,0 +1,18 @@
+"""Example transformation block for the transformation pipeline."""
+
+import dask.array as da
+
+from src.modules.transformation.verbose_transformation_block import VerboseTransformationBlock
+
+
+class ExampleTransformationBlock(VerboseTransformationBlock):
+    """An example transformation block for the transformation pipeline."""
+
+    def custom_transform(self, data: da.Array) -> da.Array:
+        """Apply a custom transformation to the data.
+
+        :param data: The data to transform
+        :param kwargs: Any additional arguments
+        :return: The transformed data
+        """
+        return data
--- src/modules/transformation/verbose_transformation_block.py
+++ src/modules/transformation/verbose_transformation_block.py
@@ -1,6 +1,4 @@
 """A verbose transformation block that logs to the terminal and to W&B."""
-from abc import abstractmethod
-from typing import Any
 
 from epochalyst.pipeline.model.transformation.transformation_block import TransformationBlock
 
@@ -13,13 +11,3 @@ class VerboseTransformationBlock(TransformationBlock, Logger):
     To use this block, inherit and implement the following methods:
     - custom_transform(x: Any, **kwargs: Any) -> Any
     """
-
-    @abstractmethod
-    def custom_transform(self, data: Any, **kwargs: Any) -> Any:  # noqa: ANN401
-        """Apply a custom transformation to the data.
-
-        :param data: The data to transform
-        :param kwargs: Any additional arguments
-        :return: The transformed data
-        """
-        return super().custom_transform(data, **kwargs)
