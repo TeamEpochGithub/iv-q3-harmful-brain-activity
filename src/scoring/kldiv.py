f055deafe876b8031482965844cdaec2759bceb5
hjdeheer <hugodeheer1234@gmail.com>
2024-03-11 15:05:39 +0100

    Update kldiv.py

--- src/scoring/kldiv.py
+++ src/scoring/kldiv.py
@@ -16,12 +16,14 @@ class KLDiv(Scorer):
         super().__init__(name)
 
     def __call__(self, y_true: np.ndarray[Any, Any], y_pred: np.ndarray[Any, Any]) -> float:
-        # Convert both to torch tensors
-
         # For each row, make sure the sum of the labels is 1
         y_true = y_true / y_true.sum(axis=1)[:, None]
+
+        # Convert both to torch tensors
         input = torch.tensor(y_pred)
         target = torch.tensor(y_true)
+
+        # Calculate the KLDivLoss
         criterion = KLDivLoss(reduction='batchmean')
         return criterion(torch.log(torch.clamp(input, min=10 ** -15, max=1 - 10 ** -15)), target)
 
