2781272eec003bf049822ab6aa06f4a1e6edd607
hjdeheer <hugodeheer1234@gmail.com>
2024-03-11 10:58:37 +0100

    Implement kldiv scorer

---
+++ src/scoring/kldiv.py
@@ -0,0 +1,27 @@
+""" KLDiv scorer class. """
+from typing import Any
+
+import numpy as np
+import torch
+from torch.nn import KLDivLoss
+
+from src.scoring.scorer import Scorer
+
+
+class KlDiv(Scorer):
+    """Abstract scorer class from which other scorers inherit from."""
+
+    def __init__(self, name: str) -> None:
+        """Initialize the scorer with a name."""
+        self.name = name
+
+    def __call__(self, y_true: np.ndarray[Any, Any], y_pred: np.ndarray[Any, Any]) -> float:
+        # Convert both to torch tensors
+        input = torch.tensor(y_pred)
+        target = torch.tensor(y_true)
+        criterion = KLDivLoss(reduction='batchmean')
+        return criterion(torch.log(torch.clamp(input, min=10 ** -15, max=1 - 10 ** -15)), target)
+
+    def __str__(self) -> str:
+        """Return the name of the scorer."""
+        return self.name
