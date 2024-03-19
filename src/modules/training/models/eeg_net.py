285a848e1cd08dadc55ad20b26577472ba0b61f2
Schobbejak <jmvanselm@gmail.com>
2024-03-19 14:29:09 +0100

    New model

---
+++ src/modules/training/models/eeg_net.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+
+class ResNet_1D_Block(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, downsampling: int):
+        super(ResNet_1D_Block, self).__init__()
+        self.bn1 == nn.BatchNorm1d(num_features=in_channels)
+        self.bn2 == nn.BatchNorm1d(num_features=out_channels)
+        self.relu = nn.ReLU(inplace=False)
+        self.dropout = nn.dropout(p=0.0, inplace=False)
+        self.conv1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.conv2 = nn.Conv1d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.maxpool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+        self.downsampling = downsampling
+       
+    def forward(self, x: Tensor):
+        identity = x
+
+        out = self.bn1(x)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.conv2(out)
+
+        out = self.maxpool(out)
+        identity = self.downsampling(x)
+
+        out += identity
+        return out
+
+
+class EEGNet(nn.Module):
+
+    def __init__(self, kernels: int, in_channels: int = 20, fixed_kernel_size: int = 17, num_classes: int = 6):
+        super(EEGNet, self).__init__()
+        self.kernels = kernels
+        self.planes = 24
+        self.parallel_conv = nn.ModuleList()
+        self.in_channels = in_channels
+        
+        for i, kernel_size in enumerate(list(self.kernels)):
+            sep_conv = nn.Conv1d(in_channels=in_channels, out_channels=self.planes, kernel_size=(kernel_size),
+                               stride=1, padding=0, bias=False,)
+            self.parallel_conv.append(sep_conv)
+
+        self.bn1 = nn.BatchNorm1d(num_features=self.planes)
+        self.relu = nn.ReLU(inplace=False)
+        self.conv1 = nn.Conv1d(in_channels=self.planes, out_channels=self.planes, kernel_size=fixed_kernel_size,
+                               stride=2, padding=2, bias=False)
+        self.block = self._make_resnet_layer(kernel_size=fixed_kernel_size, stride=1, padding=fixed_kernel_size//2)
+        self.bn2 = nn.BatchNorm1d(num_features=self.planes)
+        self.avgpool = nn.AvgPool1d(kernel_size=6, stride=6, padding=2)
+        self.rnn = nn.GRU(input_size=self.in_channels, hidden_size=128, num_layers=1, bidirectional=True)
+        self.fc = nn.Linear(in_features=424, out_features=num_classes)
+        self.rnn1 = nn.GRU(input_size=156, hidden_size=156, num_layers=1, bidirectional=True)
+
+    def _make_resnet_layer(self, kernel_size, stride, blocks=9, padding=0):
+        layers = []
+        downsample = None
+        base_width = self.planes
+
+        for i in range(blocks):
+            downsampling = nn.Sequential(
+                    nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
+                )
+            layers.append(ResNet_1D_Block(in_channels=self.planes, out_channels=self.planes, kernel_size=kernel_size,
+                                       stride=stride, padding=padding, downsampling=downsampling))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out_sep = []
+
+        for i in range(len(self.kernels)):
+            sep = self.parallel_conv[i](x)
+            out_sep.append(sep)
+
+        out = torch.cat(out_sep, dim=2)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv1(out)  
+
+        out = self.block(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.avgpool(out)  
+        
+        out = out.reshape(out.shape[0], -1)  
+
+        rnn_out, _ = self.rnn(x.permute(0,2, 1))
+        new_rnn_h = rnn_out[:, -1, :]  
+
+        new_out = torch.cat([out, new_rnn_h], dim=1)  
+        result = self.fc(new_out)  
+
+        return result
