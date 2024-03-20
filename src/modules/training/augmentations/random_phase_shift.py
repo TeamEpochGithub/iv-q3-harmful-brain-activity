d7fdd1b873f25c7e046c0abf8ff6ae58ee5f33c8
tolgakopar <cahittolgakopar@gmail.com>
2024-03-20 11:41:20 +0100

    Random phase shift augmentation

---
+++ src/modules/training/augmentations/random_phase_shift.py
@@ -0,0 +1,23 @@
+"""Randomly shift the phase of all the frequencies frequencies."""
+from dataclasses import dataclass
+import torch
+import numpy as np
+
+@dataclass
+class RandomPhaseShift():
+    """Randomly shift the phase of all the frequencies frequencies."""
+    shift_limit: float = 0.25
+        
+        
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply Random phase shift to each frequency of the fft of the input signal."""
+        # take the rfft of the input tensor
+        x_freq = torch.fft.rfft(x)
+        # create a random array of complex numbers each with a random pahse but with magnitude of 1
+        random_phase = torch.rand(*x_freq.shape, device=x.device, dtype=x.dtype) * 2 * np.pi * self.shift_limit
+        random_phase = torch.cos(random_phase) + 1j * torch.sin(random_phase)
+        # multiply the rfft with the random phase
+        x_freq = x_freq * random_phase
+        # take the irfft of the result
+        phase_shifted = torch.fft.irfft(x_freq)
+        return phase_shifted
