303d12ae70263c42dffb83333933e0153db76a14
tolgakopar <cahittolgakopar@gmail.com>
2024-03-21 11:35:47 +0100

    pre-commit changes

--- src/modules/training/losses/gaussian_kldiv.py
+++ src/modules/training/losses/gaussian_kldiv.py
@@ -6,23 +6,25 @@ from torch.nn import KLDivLoss
 
 
 class GaussianKLDivLoss(nn.Module):
-    """Custom KLDivLoss class. Wrapper for the KLDivLoss function from PyTorch."""
+    """KLDivLoss class. Used for gaussian predictions without a softmax layer."""
 
-    def __init__(self, reduction: str = "batchmean", weighted: bool = False) -> None:  # noqa: FBT001, FBT002
-        """Initialize the CustomKLDivLoss class.
+    def __init__(self, reduction: str = "batchmean") -> None:
+        """Initialize the class.
 
         :param reduction: The reduction method to use. Default is "batchmean".
-        :parapm
         """
         super().__init__()
         self.reduction = reduction
-        self.weighted = weighted
 
     def forward(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the CustomKLDivLoss class."""
+        """Do not use softmax outputs forthis loss function. Forward pass of the CustomKLDivLoss class.
+
+        :param pred: The predicted values.
+        :param target: The target values.
+        :return: The loss value.
+        """
         criterion = KLDivLoss(reduction=self.reduction)
 
         # Calculate the KLDivLoss
-        loss = criterion(torch.log_softmax(torch.clamp(pred, min=10**-15, max=1 - 10**-15)), torch.softmax(target, dim=1))
 
-        return loss
+        return criterion(torch.log_softmax(torch.clamp(pred, min=10**-15, max=1 - 10**-15), dim=1), torch.softmax(target, dim=1))
