20c4ba6063356999abcbee487e926972e42a48fd
Schobbejak <jmvanselm@gmail.com>
2024-03-22 12:24:40 +0100

    Add early stopping to cv

--- cv.py
+++ cv.py
@@ -112,6 +112,8 @@ def run_cv_cfg(cfg: DictConfig) -> None:
         scores.append(score)
         accuracies.append(accuracy)
         f1s.append(f1)
+        if score > 0.85:
+            break
 
     avg_score = np.average(np.array(scores))
     avg_accuracy = np.average(np.array(accuracies))
