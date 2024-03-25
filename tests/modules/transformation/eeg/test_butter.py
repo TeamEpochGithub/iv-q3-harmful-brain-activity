476410124da100388cbafdf6a268072dffcf304f
Emiel Witting <emielwitting@teamepoch.net>
2024-03-25 17:45:04 +0100

    update tests

--- tests/modules/transformation/eeg/test_butter.py
+++ tests/modules/transformation/eeg/test_butter.py
@@ -8,50 +8,40 @@ from src.modules.transformation.eeg.butter import ButterFilter
 from src.typing.typing import XData
 
 
+def generate_sine(frequency: float, duration: float, sample_rate: int) -> np.ndarray:
+    """Generate a sine wave."""
+    x = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    return np.sin(2 * np.pi * frequency * x)
+
+
 def setup_data() -> XData:
     eeg = {
-        0: pd.DataFrame([1, 2, 3, 4, 5]),
-        1: pd.DataFrame([6, 7, 8, 9, 10]),
+        0: pd.DataFrame(generate_sine(15, 5, 200)),
+        1: pd.DataFrame(generate_sine(0.1, 5, 200)),
     }
     meta = pd.DataFrame([1, 2, 3, 4, 5])
     return XData(eeg=eeg, kaggle_spec=None, eeg_spec=None, meta=meta, shared=None)
 
 
-def expected_data_low() -> XData:
-    data = setup_data()
-    eegs = data.eeg
-    butter = ButterFilter(0, 20, 2, 200)
-    for key in eegs:
-        eeg = eegs[key]
-        for col in eeg.columns:
-            eeg[col] = butter.butter_lowpass_filter(eeg[col])
-    return data
-
-
-def expected_data_band() -> XData:
-    data = setup_data()
-    eegs = data.eeg
-    butter = ButterFilter(0.5, 20, 2, 200)
-    for key in eegs:
-        eeg = eegs[key]
-        for col in eeg.columns:
-            eeg[col] = butter.butter_bandpass_filter(eeg[col])
-    return data
-
-
 class TestButterFilter(TestCase):
     def test_lowpass(self):
         data = setup_data()
-        butter = ButterFilter(0, 20, 2, 200)
+        butter = ButterFilter(0, 5, 6, 200)
         eeg = butter.transform(data).eeg
-        expected = expected_data_low().eeg
-        for key in eeg.keys():
-            pd.testing.assert_frame_equal(eeg[key], expected[key])
+
+        # high frequency should be mostly gone
+        self.assertTrue(np.isclose(eeg[0], 0.0, atol=0.15).all())
+
+        # low frequency should mostly still exist
+        self.assertTrue(np.isclose(eeg[1].values[:,0], generate_sine(0.1, 5, 200), atol=0.2).all())
 
     def test_bandpass(self):
         data = setup_data()
-        butter = ButterFilter(0.5, 20, 2, 200)
+        butter = ButterFilter(5, 7, 6, 200)
         eeg = butter.transform(data).eeg
-        expected = expected_data_band().eeg
-        for key in eeg.keys():
-            pd.testing.assert_frame_equal(eeg[key], expected[key])
+
+        # high frequency should be mostly gone
+        self.assertTrue(np.isclose(eeg[0], 0.0, atol=0.1).all())
+
+        # low frequency should be gone
+        self.assertTrue(np.isclose(eeg[1], 0.0, atol=0.01).all())
