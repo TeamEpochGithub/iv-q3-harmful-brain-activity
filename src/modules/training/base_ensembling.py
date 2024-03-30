ff1675fe32de9d34079b42f96f815e504c24162e
schobbejak <jmvanselm@gmail.com>
2024-03-30 15:42:58 +0100

    Update ensemble for new epochalyst version

--- src/modules/training/base_ensembling.py
+++ src/modules/training/base_ensembling.py
@@ -2,6 +2,6 @@ from epochalyst.pipeline.ensemble import EnsemblePipeline
 
 
 class BaseEnsemble(EnsemblePipeline):
-
-    def concat(self, original_data, new_data, weight):
-        return original_data + new_data * weight
+    """BaseEnsemble pipeline for other ensembling methods override concat"""
+    # def concat(self, original_data, new_data, weight):
+    #     return original_data + new_data * weight
